184be575b47a17dbd2a33d7b70cd91a916d94900
prachisingh2 <68850178+prachisingh2@users.noreply.github.com>
2024-09-10 23:53:21 +0530

    Update params.py

--- semanticSearch/atlas-langchain/params.py
+++ semanticSearch/atlas-langchain/params.py
@@ -1,6 +1,6 @@
-openai_api_key = "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"
+openai_api_key = "sk-sAKCrOHCgygHTymp"
 
-mongodb_conn_string = "mongodb+srv://prachisngrwl:zxOC6l7RrquHU6Bz@cluster0.dmxrhed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongodb_conn_string = "mongodb+srv://prachisngrwl:<pass>@cluster0.dmxrhed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 db_name = "search_db1"
 collection_name = "search_col3"
